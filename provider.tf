5d79361f6a6490acf4dd3a52ecb8bc554c17550f
Jonas Tatchieup <tjonas02@gmail.com>
2024-11-01 08:23:32 -0400

    change provider

--- provider.tf
+++ provider.tf
@@ -1,9 +1,12 @@
-#terraform {
- # required_version = "~> 1.9.8"
-  #required_providers {
-   # aws = {
-    #  source  = "hashicorp/aws"
-     # version = ">= 5.0.0"
-    #}
-  #}
-#}
+terraform {
+  required_version = "~> 1.9.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
